dded646ea2c4fb7be1d5c777e8f3afad0322b2ac
kietran <trantuankjet12a2hb@gmail.com>
2021-05-18 09:17:55 +0700

    update ExportImportExcel

---
+++ ExportImportExcel/Controllers/ImageInfoController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using ClosedXML.Excel;
+using ExportImportExcel.Models;
+using OfficeOpenXml;
+using System.Data;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using DataTable = System.Data.DataTable;
+
+namespace ExportImportExcel.Controllers
+{
+    public class ImageInfoController : Controller
+    {
+        ImageEntities1 db = new ImageEntities1();
+        //public int Department { get; set; }
+        //public IEnumerable<ActualLabel> Departments { get; set; }
+        // GET: ImageInfo
+        public ActionResult Index()
+        {
+
+            using (db = new ImageEntities1())
+            {
+                dynamic model = new List<ExpandoObject>();
+                List<ActualLabel> cate = db.ActualLabels.ToList();
+                SelectList cateList = new SelectList(cate, "id", "actual_label_name");
+                ViewBag.CategoryList = cateList;
+
+                // lay du lieu bang ImageInfoes from image in db.ImageInfoes
+                // join voi db.ActualLabels 
+                // dieu kien join image.actual_label_id equals actual.id 
+                var images = (from image in db.ImageInfoes
+                    join  actual in db.ActualLabels on image.actual_label_id equals actual.id into  gj
+                    from subpet in gj.DefaultIfEmpty()
+                    select new
+                    {
+                        image.id,
+                        image.image_id,
+                        image.image_link,
+                        image.predict_label,
+                        image.actual_label_id,
+                        subpet.actual_label_name
+                    }).ToList();
+
+                foreach (var image in images)
+                {
+                    dynamic img = new ExpandoObject();
+                    img.id = image.id;
+                    img.image_id = image.image_id;
+                    img.image_link = image.image_link;
+                    img.predict_label = image.predict_label;
+                    img.actual_label_id = image.actual_label_id;;
+                    img.actual_label_name = image.actual_label_name;
+                    
+
+                    model.Add(img);
+                }
+
+                
+                /*lay danh sach list ActualLabel
+                 */
+                //var actualLabelList = (from actual in db.ActualLabels
+                //                       select new
+                //                       {
+                //                           actual.id,
+                //                           actual.actual_label_name
+                //                       }).ToList();
+                //foreach (var labe in actualLabelList)
+                //{
+                //    dynamic actualLabel = new ExpandoObject();
+                //    actualLabel.id = labe.id;
+                //    actualLabel.actual_label_name = labe.actual_label_name;
+                //    cateList.Add(actualLabel);
+                //    return View(cateList);
+                //}
+                //ViewData["allData"] = images;
+                //ViewData["actualLabelList"] = actualLabelList;
+                return View(model);
+            }
+
+        }
+
+        [HttpPost]
+        public FileResult ExportToExcel()
+        {
+            DataTable dt = new DataTable("Grid");
+            dt.Columns.AddRange(new DataColumn[4]
+            {
+                new DataColumn("id"),
+                new DataColumn("image_id"),
+                new DataColumn("image_link"),
+                new DataColumn("predict_label")
+            });
+            var imageInfo = from ImageInfo in db.ImageInfoes select ImageInfo;
+            foreach (var img in imageInfo)
+            {
+                dt.Rows.Add(img.image_id, img.image_link, img.predict_label);
+            }
+            using (XLWorkbook wb = new XLWorkbook()) //Install ClosedXml from Nuget for XLWorkbook  
+            {
+                wb.Worksheets.Add(dt);
+                using (MemoryStream stream = new MemoryStream()) //using System.IO;  
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ExcelFile.xlsx");
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ImportFromExcel(HttpPostedFileBase postedFile)
+        {
+            
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["postedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+                    ExcelPackage.LicenseContext = LicenseContext.Commercial;
+                    using (var package = new ExcelPackage(file.InputStream))
+                    {
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First(); if (workSheet != null)
+                        {
+                            using (var imageContext = new ImageEntities1())
+                            {
+                                var usersList = new List<ImageInfo>();
+
+                                // số hàng
+                                var rows = workSheet.Dimension.Rows;
+                                // số cột
+                                var column = workSheet.Dimension.Columns;
+
+                                for (int i = 2; i <= rows; i++)
+                                {
+                                    if (workSheet.Cells[i, 1].Value != null)
+                                    {
+                                        var imageId = int.Parse(workSheet.Cells[i, 1].Value.ToString());
+                                        var imageLink = workSheet.Cells[i, 2].Value.ToString();
+                                        var PredictLabel = workSheet.Cells[i, 3].Value.ToString();
+
+                                        var pictureInformation = new ImageInfo
+                                        {
+                                            image_id = imageId,
+                                            image_link = imageLink,
+                                            predict_label = PredictLabel
+                                        };
+                                        usersList.Add(pictureInformation);
+                                    } 
+                                }
+
+                                imageContext.ImageInfoes.AddRange(usersList);
+                                imageContext.SaveChanges();
+                                var imageInfos = db.ImageInfoes.ToList();
+                                return View("imageInfos");
+                            }    
+                            
+                            
+                        }
+                       
+                    }
+                }
+            }
+            return Json("no files were selected !");
+        }
+
+
+    }
+}
---
+++ ExportImportExcel/Controllers/TuannvController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OfficeOpenXml;
+
+namespace ExportImportExcel.Controllers
+{
+    public class TuannvController : Controller
+    {
+        // GET: Tuannv
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+
+            // file se nhan dc o day
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+
+                    // khởi tạo 
+                    var excelPackage = new ExcelPackage();
+                    // load file vào đối tượng
+                    excelPackage.Load(file.InputStream);
+                    // lấy ra sheet đầu tiên
+                    var workSheet = excelPackage.Workbook.Worksheets.FirstOrDefault();
+                    if (workSheet != null)
+                    {
+                        // số hàng
+                        var rows = workSheet.Dimension.Rows;
+                        // số cột
+                        var column = workSheet.Dimension.Columns;
+
+                        // lấy giá trị hàng 1 cột 1, trong excel ko có khái niệm hàng số 0, cột 0
+                        // bắt đầu từ 1
+                        var cell = workSheet.Cells[1, 1].Value.ToString();
+                    }
+
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+        }
+    }
+}
